702838ab2832d6a2b2b48141818750c9fb6f3bcb
OroshiX <oroshix@users.noreply.github.com>
2018-01-16 10:36:38 +0100

    fonts added

---
+++ app/src/main/java/com/nimoroshix/pentatonic/AboutActivity.kt
@@ -0,0 +1,11 @@
+package com.nimoroshix.pentatonic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
