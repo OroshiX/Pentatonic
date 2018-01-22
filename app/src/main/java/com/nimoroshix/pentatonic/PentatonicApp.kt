6f4f7d744b2c2849c5c47f5e518ec239edc09bc7
OroshiX <oroshix@users.noreply.github.com>
2018-01-22 17:11:09 +0100

    Different ways of checking for upgrades (maybe remove the upgrade receiver and only take the application way)
    Signed-off-by: OroshiX <oroshix@users.noreply.github.com>

--- app/src/main/java/com/nimoroshix/pentatonic/PentatonicApp.kt
+++ app/src/main/java/com/nimoroshix/pentatonic/PentatonicApp.kt
@@ -1,6 +1,8 @@
 package com.nimoroshix.pentatonic
 
 import android.app.Application
+import android.content.Context
+import android.util.Log
 import com.facebook.stetho.Stetho
 
 /**
@@ -8,10 +10,45 @@ import com.facebook.stetho.Stetho
  * Created by OroshiX on 13/01/2018.
  */
 class PentatonicApp : Application() {
+
     override fun onCreate() {
         super.onCreate()
         Stetho.initialize(Stetho.newInitializerBuilder(this).enableDumpapp(
                 Stetho.defaultDumperPluginsProvider(this))
                 .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this)).build())
+
+        checkFirstRun()
+    }
+
+    private fun checkFirstRun() {
+        // get Current version code
+        val currentVersionCode = BuildConfig.VERSION_CODE
+
+        // get saved version code
+        val prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val savedVersionCode = prefs.getInt(PREF_VERSION_CODE_KEY, DOESNT_EXIST)
+
+        Log.d(TAG, "current version : $currentVersionCode, saved version : $savedVersionCode")
+
+        // check for first run or upgrade
+        if (currentVersionCode == savedVersionCode) {
+            // Just a normal run
+            return
+        } else if (savedVersionCode == DOESNT_EXIST) {
+            // TODO this is a new install (or user cleared the sharedPrefs)
+        } else if (currentVersionCode > savedVersionCode) {
+            // TODO this is an upgrade (handle different migrations)
+        }
+
+        // update the shared prefs with the current version code
+        prefs.edit().putInt(PREF_VERSION_CODE_KEY, currentVersionCode).apply()
+    }
+
+    companion object {
+        const val TAG = "PentatonicApp"
+        const val PREFS_NAME = "pentatonicPrefFile"
+        const val PREF_VERSION_CODE_KEY = "version_code"
+        const val DOESNT_EXIST = -1
+
     }
 }
---
+++ app/src/main/java/com/nimoroshix/pentatonic/UpgradeReceiver.kt
@@ -0,0 +1,31 @@
+package com.nimoroshix.pentatonic;
+
+import android.content.BroadcastReceiver;
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+/**
+ * Project Pentatonic
+ * <p>
+ * Created by OroshiX on 22/01/2018.
+ */
+
+class UpgradeReceiver : BroadcastReceiver() {
+    companion object {
+        const val TAG = "UpgradeReceiver"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // Application was upgraded
+        Log.d(TAG, "onReceive: ${intent.action}")
+        val versionCode = BuildConfig.VERSION_CODE;
+        val versionName = BuildConfig.VERSION_NAME;
+
+        val packageName = intent.data
+        if (packageName.toString() == "package:${context.packageName}") {
+            // Application was upgraded
+
+        }
+    }
+}
