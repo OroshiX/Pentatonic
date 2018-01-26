064ead740f06744666bcc47fb7779d52a8823ec7
OroshiX <oroshix@users.noreply.github.com>
2018-01-26 15:33:04 +0100

    Cleanup SettingsActivity
    Signed-off-by: OroshiX <oroshix@users.noreply.github.com>

--- app/src/main/java/com/nimoroshix/pentatonic/SettingsActivity.kt
+++ app/src/main/java/com/nimoroshix/pentatonic/SettingsActivity.kt
@@ -173,45 +173,43 @@ class SettingsActivity : AppCompatPreferenceActivity() {
         private val sBindPreferenceSummaryToValueListener = Preference.OnPreferenceChangeListener { preference, value ->
             val stringValue = value.toString()
 
-            if (preference is ListPreference) {
-                // For list preferences, look up the correct display value in
-                // the preference's 'entries' list.
-                val listPreference = preference
-                val index = listPreference.findIndexOfValue(stringValue)
+            when (preference) {
+                is ListPreference -> {
+                    // For list preferences, look up the correct display value in
+                    // the preference's 'entries' list.
+                    val index = preference.findIndexOfValue(stringValue)
 
-                // Set the summary to reflect the new value.
-                preference.setSummary(
-                        if (index >= 0)
-                            listPreference.entries[index]
-                        else
-                            null)
+                    // Set the summary to reflect the new value.
+                    preference.setSummary(
+                            if (index >= 0)
+                                preference.entries[index]
+                            else
+                                null)
 
-            } else if (preference is RingtonePreference) {
-                // For ringtone preferences, look up the correct display value
-                // using RingtoneManager.
-                if (TextUtils.isEmpty(stringValue)) {
-                    // Empty values correspond to 'silent' (no ringtone).
-                    preference.setSummary(R.string.pref_ringtone_silent)
-
-                } else {
-                    val ringtone = RingtoneManager.getRingtone(
-                            preference.getContext(), Uri.parse(stringValue))
-
-                    if (ringtone == null) {
-                        // Clear the summary if there was a lookup error.
-                        preference.setSummary(null)
-                    } else {
-                        // Set the summary to reflect the new ringtone display
-                        // name.
-                        val name = ringtone.getTitle(preference.getContext())
-                        preference.setSummary(name)
-                    }
                 }
+                is RingtonePreference -> // For ringtone preferences, look up the correct display value
+                    // using RingtoneManager.
+                    if (TextUtils.isEmpty(stringValue)) {
+                        // Empty values correspond to 'silent' (no ringtone).
+                        preference.setSummary(R.string.pref_ringtone_silent)
 
-            } else {
-                // For all other preferences, set the summary to the value's
-                // simple string representation.
-                preference.summary = stringValue
+                    } else {
+                        val ringtone = RingtoneManager.getRingtone(
+                                preference.getContext(), Uri.parse(stringValue))
+
+                        if (ringtone == null) {
+                            // Clear the summary if there was a lookup error.
+                            preference.setSummary(null)
+                        } else {
+                            // Set the summary to reflect the new ringtone display
+                            // name.
+                            val name = ringtone.getTitle(preference.getContext())
+                            preference.setSummary(name)
+                        }
+                    }
+                else -> // For all other preferences, set the summary to the value's
+                    // simple string representation.
+                    preference.summary = stringValue
             }
             true
         }
